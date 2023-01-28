d3570a4606eb54cb2418e4d05523d87ba0b3ae5b
Francesco Frassineti <frassybhik@gmail.com>
2023-01-28 15:33:03 +0100

    +

--- js/anti-reddit.js
+++ js/anti-reddit.js
@@ -4,8 +4,4 @@ const antiRedditCss = "css/anti-reddit.css";
 if (!window.location.href.includes("/r/eurovision")) {
     // Redirect the user to the Eurovision Subreddit
     window.location.href = "https://www.reddit.com/r/eurovision/";
-    console.log("E");
-}
-else{
-    console.log("A");
 }
